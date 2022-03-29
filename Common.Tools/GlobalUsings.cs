03fa22ba84e38ec5077433ff3394b359f1a2ee05
HenrikDK <commander.john.crichton@gmail.com>
2022-03-30 00:47:51 +0200

    add global usings

---
+++ Common.Tools/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using Microsoft.Data.SqlClient;
+global using Microsoft.Extensions.Logging;
+
+global using System.Diagnostics;
+global using System.Collections.Generic;
+global using System.Data;
+global using System;
+global using System.Reflection;
+global using System.Transactions;
+
+global using Prometheus;
